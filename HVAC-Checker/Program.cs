d72fcc8639ee9a0363dd1691b9e9d4a799233527
zhenyi100056 <zhenyi100056@126.com>
2020-02-16 23:06:56 +0800

    同步

--- HVAC-Checker/Program.cs
+++ HVAC-Checker/Program.cs
@@ -25,8 +25,12 @@ namespace HVAC_Checker
         {
          
             dynamic type = (new Program()).GetType();
-            string currentDirectory = Path.GetDirectoryName(type.Assembly.Location);
             
+            string currentDirectory = Path.GetDirectoryName(type.Assembly.Location);
+            int iSub = currentDirectory.IndexOf("\\bin");
+            currentDirectory = currentDirectory.Substring(0, iSub);
+
+
             string path = new DirectoryInfo(currentDirectory + "/机电.GDB").FullName;
             
             //如果不存在，则创建一个空的数据库,
@@ -34,7 +38,9 @@ namespace HVAC_Checker
                 SQLiteConnection.CreateFile("MyDatabase.sqlite");
 
             //创建一个连接
-            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=机电.GDB;Version=3;");
+            string connectionstr = @"data source =" + path;
+            SQLiteConnection m_dbConnection = new SQLiteConnection(connectionstr);
+           // SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=机电.GDB;Version=3;");
             m_dbConnection.Open();
 
             ////创建一个数据表
@@ -68,6 +74,14 @@ namespace HVAC_Checker
             //SQLiteDataReader reader = command.ExecuteReader();
             //while (reader.Read())
             //    Console.WriteLine("书名: " + reader["title"] + "\t页数: " + reader["pages"]);
+
+
+            //使用sql查询语句，并显示结果
+            string sql = "select * from AirTerminals";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())          
+                Console.WriteLine("书名: " + reader["AirTerminalType"]  );
 
             //关闭连接
             m_dbConnection.Close();
