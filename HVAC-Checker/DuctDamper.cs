2138b7b47dbf222e85cf9d408b3aa0867d7e5444
me <15811560065@163.com>
2020-04-20 10:36:12 +0800

    重置分支

---
+++ HVAC-Checker/DuctDamper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HVAC_CheckEngine
+{ 
+    public class DuctDamper : Element
+    {
+        public DuctDamper(long id) : base(id)
+        {
+
+        }
+        public double? airVelocity { get; set; } = null;
+
+    }
+}
